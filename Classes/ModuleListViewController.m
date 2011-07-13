7c7576bb74fc5c20b784f6b7f8aef451789fe083
zhangying <zhangying900815@gmail.com>
2011-07-13 21:44:23 +0800

    continue works

--- Classes/ModuleListViewController.m
+++ Classes/ModuleListViewController.m
@@ -85,25 +85,6 @@
 }
 
 
-- (void)viewWillAppear:(BOOL)animated{
-	[self.view becomeFirstResponder];
-	[moduleListTableView resignFirstResponder];
-	
-	SharedAppDataObject* theDataObject = [self theAppDataObject];
-	
-	for (NSString *key in theDataObject.removedCells) {
-		NSIndexPath *path = [theDataObject.removedCells objectForKey:key];
-		UITableViewCell *cell = [moduleListTableView cellForRowAtIndexPath:path];
-		UIButton *button = (UIButton *)cell.accessoryView;
-		
-		UIImage *newImage = [UIImage imageNamed:@"unchecked.png"];
-		[button setBackgroundImage:newImage forState:UIControlStateNormal];
-	}
-	[theDataObject.removedCells removeAllObjects];
-	[moduleListTableView reloadData];
-}
-
-
 - (id)initWithTabBar{
 	if (self = [super initWithNibName:@"ModuleListViewController" bundle:nil]){
 		self.title = @"Modules";
@@ -454,21 +435,11 @@
 	//TODO: requirements part (present model view for requirements, after done, dismiss and then call sync and then to cal
 	SharedAppDataObject* theDataObject = [self theAppDataObject];
 	theDataObject.continueToCalendar = NO;
-	NSLog(theDataObject.continueToCalendar?@"c: Y":@"c: N");
 	RequirementPlacingViewController *reqController = [[RequirementPlacingViewController alloc] initWithStyle:UITableViewStyleGrouped];
 	UINavigationController *navController = [[UINavigationController alloc] initWithRootViewController:reqController];
 	[reqController release];
     [[self navigationController] presentModalViewController:navController animated:YES];
     [navController release];
-	if (theDataObject.continueToCalendar == YES){
-		// TODO: call the model logic stuff here....
-		NSLog(theDataObject.continueToCalendar?@"continue: Y":@"cancel: N");
-		self.tabBarController.selectedViewController = [self.tabBarController.viewControllers objectAtIndex:0];	
-	}else {
-		NSLog(theDataObject.continueToCalendar?@"cancel: Y":@"cancel: N");
-		// do nothing
-	}
-
 }
 
 
@@ -497,6 +468,37 @@
 	[moduleListTableView becomeFirstResponder];
 	[searchBar setShowsCancelButton:NO];
     [searchBar resignFirstResponder];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+	[super viewWillAppear:animated];
+	
+	[self.view becomeFirstResponder];
+	[moduleListTableView resignFirstResponder];
+	
+	SharedAppDataObject* theDataObject = [self theAppDataObject];
+	
+	for (NSString *key in theDataObject.removedCells) {
+		NSIndexPath *path = [theDataObject.removedCells objectForKey:key];
+		UITableViewCell *cell = [moduleListTableView cellForRowAtIndexPath:path];
+		UIButton *button = (UIButton *)cell.accessoryView;
+		
+		UIImage *newImage = [UIImage imageNamed:@"unchecked.png"];
+		[button setBackgroundImage:newImage forState:UIControlStateNormal];
+	}
+	[theDataObject.removedCells removeAllObjects];
+	[moduleListTableView reloadData];
+	
+	// check whether user clicks cancel or continue in the requirements view
+	if (theDataObject.continueToCalendar == YES){
+		// TODO: call the model logic stuff here....
+		theDataObject.continueToCalendar = NO;
+		self.tabBarController.selectedViewController = [self.tabBarController.viewControllers objectAtIndex:0];	
+	}else {
+		theDataObject.continueToCalendar = NO;
+		//NSLog(theDataObject.continueToCalendar?@"cancel: Y":@"cancel: N");
+		// do nothing
+	}
 }
 
 - (void)dealloc {
